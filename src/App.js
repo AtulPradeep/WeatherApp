d9ef962e0c77fa16768e351200c12a49dff625c3
Atul Pradeep <atul.pradeep40@gmail.com>
2022-05-10 22:29:06 +0200

    last

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { useState , useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = {
+  key: "5085bd714496c68fd9b8f64f43b8b1c1",
+  base: "https://api.openweathermap.org/data/2.5/"
 }
+
+function App() {
+const[city,setCity] = useState('');
+const[temp,setTemp] = useState("");
+const[feels,setfeels] = useState("");
+const [query, setQuery] = useState('');
+const [input,setInput] =useState("");
+
+const getData =async (qs="hannover")=>{
+  const response = await fetch(`${api.base}weather?q=${qs}&units=metric&APPID=${api.key}`);
+  const weathers = await response.json();
+  const city = weathers.name;
+  const temp = weathers.main.temp;
+  const fe = weathers.main.feels_like;
+  console.log(weathers);
+
+
+  setCity(city);
+  setTemp(temp);
+  setfeels(fe);
+}
+useEffect(() => {
+  getData();
+ 
+  },[]);
+
+  const handlesubmit =(e)=>{
+    e.preventDefault();
+    setQuery(input);
+    let qs = input;
+    getData(qs);
+  }
+const onChange =(e)=>{
+  e.preventDefault();
+  setInput(e.target.value);
+}
+
+ return (
+    <div className="App">
+      <div className ="weather-box">
+     <h1>{city}</h1>
+     <h2>{Math.round(temp)}° </h2>
+
+     <h6>Feels like : {Math.round(feels)}° </h6>
+     </div>
+     <form onSubmit ={handlesubmit} >
+     <input 
+            type="text"
+            className="search-bar"
+            placeholder="Enter city name"
+            onChange={onChange}
+            value={input}
+            
+         
+         />
+         
+    </form>
+
+
+     
+  </div>
+  );
+  }
+  
 
 export default App;
